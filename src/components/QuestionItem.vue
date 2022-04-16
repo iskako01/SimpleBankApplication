897dd27e70da3733921b49b9432f90cb4bdc015d
iskako01 <85966697+iskako01@users.noreply.github.com>
2022-04-16 20:46:00 +0200

    Help component

---
+++ src/components/QuestionItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="question" @click="isOpen = !isOpen">
+    <div class="question-title">{{ questions.title }}</div>
+    <div class="question-body" v-if="isOpen">{{ questions.text }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "QuestionItem",
+  props: {
+    questions: {
+      type: Object,
+      required: true,
+    },
+  },
+  setup() {
+    const isOpen = ref(false);
+
+    return { isOpen };
+  },
+});
+</script>
--- src/views/Help.vue
+++ src/views/Help.vue
@@ -1,15 +1,35 @@
 <template>
-  <div class=""></div>
+  <AppPage back title="Help">
+    <QuestionItem
+      v-for="question in questions"
+      :key="question.title"
+      :questions="question"
+    ></QuestionItem>
+  </AppPage>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import AppPage from "@/components/AppPage.vue";
+import QuestionItem from "@/components/QuestionItem.vue";
+import Iquestions from "@/types/questions/questions";
 
 export default defineComponent({
+  name: "Help",
+  components: { AppPage, QuestionItem },
   setup() {
-    return {};
+    const questions: Iquestions[] = [
+      {
+        title: "What is in this system ?",
+        text: "There is everything to see what requests a person has added ",
+      },
+      {
+        title: "How does authorization work here? ",
+        text: "All implamented through Firebase ",
+      },
+    ];
+
+    return { questions };
   },
 });
 </script>
-
-<style></style>
